7f8547a4ba92890f6918f5455b18def9715c8b36
Karlan Lemmen <karlanlemmen@gmail.com>
2024-12-17 16:54:13 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QX3ivCumcxrmNTlfi1T0ZVhfPenQGY25gTH9pzWZogjM79WnDObjT0iugnUGNp1wzAl0HXFpmdH6DHpKk65IQzM00zeBTBH32',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
